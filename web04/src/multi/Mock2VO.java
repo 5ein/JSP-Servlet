417724b21922c443d7f9a9864535adbed3c36046
5ein <124111027+5ein@users.noreply.github.com>
2023-03-24 17:26:28 +0900

    Add files via upload

---
+++ web04/src/multi/Mock2VO.java
@@ -0,0 +1,47 @@
+package multi;
+
+public class Mock2VO {
+	private String id;
+	private String pw;
+	private String username;
+	private String gender;
+	private String email;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Override
+	public String toString() {
+		return "Mock2VO [id=" + id + ", pw=" + pw + ", username=" + username + ", gender=" + gender + ", email=" + email
+				+ "]";
+	}
+
+}
